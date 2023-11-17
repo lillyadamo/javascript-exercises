474bbe12f48ac11baa998012da1efa489bcdc17d
lillyadamo <lillyadamo@outlook.com>
2023-11-17 11:31:29 -0600

    javascript exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(input) {
+    return input.split('').reverse().join('');
+  };
+  
+  // Example usage
+  console.log(reverseString('hello')); // Output: 'olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
